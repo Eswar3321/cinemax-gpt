c303f5ab81ce9e6213d1de1a95f7e87638f428c1
Eswar3321 <eswararao8786@gmail.com>
2025-04-19 19:06:16 +0530

    mobile view fixes

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -9,7 +9,7 @@ const VideoBackground = ({movieId}) => {
       <div className="relative w-full h-full">
         <div className="absolute inset-0 w-full h-full">
           <iframe
-            className="absolute top-1/2 left-1/2 w-[450%] h-[450%] sm:w-[400%] sm:h-[400%] md:w-[350%] md:h-[350%] lg:w-[300%] lg:h-[300%] xl:w-[250%] xl:h-[250%] -translate-x-1/2 -translate-y-1/2"
+            className="absolute top-1/2 left-1/2 w-[550%] h-[550%] sm:w-[400%] sm:h-[400%] md:w-[350%] md:h-[350%] lg:w-[300%] lg:h-[300%] xl:w-[250%] xl:h-[250%] -translate-x-1/2 -translate-y-1/2"
             src={`https://www.youtube.com/embed/${trailerVideo?.key}?autoplay=1&mute=1&loop=1&playlist=${trailerVideo?.key}`}
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
